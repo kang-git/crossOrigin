d107a663ab93431fe586de6074f2d43c11b75e29
Maxkang <2265059578@qq.com>
2023-09-24 21:57:55 +0800

    跨域演示

--- frontend/server.js
+++ frontend/server.js
@@ -11,12 +11,6 @@ app.use(views(path.join(__dirname, './'), {extension: 'html'}))
 console.log('router', router.get)
 router.get('/', demo).get('/demo.html', demo)
 
-// web服务的bff层
-// router.post('/api/task', async (ctx, next) => {
-//     const res = await axios.post('http://127.0.0.1:4000/api/task')
-//     ctx.body = res.data;
-// })
-
 async function demo(ctx) {
     await ctx.render('demo')
 }
