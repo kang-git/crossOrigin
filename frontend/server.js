3adf6cb14455ba16607f272dec90d289377d6bbd
Maxkang <2265059578@qq.com>
2023-09-24 22:06:48 +0800

    febff

--- frontend/server.js
+++ frontend/server.js
@@ -12,10 +12,10 @@ console.log('router', router.get)
 router.get('/', demo).get('/demo.html', demo)
 
 // web服务的bff层
-// router.post('/api/task', async (ctx, next) => {
-//     const res = await axios.post('http://127.0.0.1:4000/api/task')
-//     ctx.body = res.data;
-// })
+router.post('/api/task', async (ctx, next) => {
+    const res = await axios.post('http://127.0.0.1:4000/api/task')
+    ctx.body = res.data;
+})
 
 async function demo(ctx) {
     await ctx.render('demo')
